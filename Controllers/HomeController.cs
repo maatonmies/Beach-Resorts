2a9e0eb2b27ddc3082dbd646e4fde5e0285823bd
Maaton Mies <kisvardaymarton@icloud.com>
2017-06-06 19:45:04 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,13 @@
 ﻿using System.Web.Mvc;
-using HoidayResorts.DataContexts;
 
 namespace HoidayResorts.Controllers
 {
     public class HomeController : Controller
     {
-        HolidayResortsDb _db = new HolidayResortsDb();
-
         //Home Page
         public ViewResult Index()
         {
             return View();
-        }
-
-        //Dispose database
-        protected override void Dispose(bool disposing)
-        {
-            _db?.Dispose();
-
-            base.Dispose(disposing);
         }
     }
 }
